c851977b1beac3f622ca4a0312496766c330ecd9
ApamAa <apama.jamshidi@gmail.com>
2019-12-06 05:31:03 +0000

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+/*fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -8,3 +8,15 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+*/
+
+
+fetchCoordsByIP("198.47.47.207", (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
+});
+
